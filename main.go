9842665e56046de7096ecf223bb8b219db60c3f8
iammadab <iammadab@gmail.com>
2021-06-13 14:04:36 +0000

    Reprensenting shares as points struct

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
-	"time"
+	//"time"
 )
+
+type Point struct {
+	X int
+	Y int
+}
 
 // n - number of shares to generate
 // k - number of shares needed to reconstruct the secret
 // TODO: Use a more secure random number generator
-func makeShares(secret, k, n int) []int {
+func makeShares(secret, k, n int) []Point {
 	var curve = make([]int, k)
 	for i := 0; i < k; i++ {
 		if i == 0 {
@@ -20,27 +25,23 @@ func makeShares(secret, k, n int) []int {
 		curve[i] = rand.Intn(secret)
 	}
 	fmt.Println(curve)
-	var shares = make([]int, n)
+	var shares = make([]Point, n)
 	for i := 0; i < n; i++ {
 		shares[i] = evaluatePolynomial(curve, i+1)
 	}
 	return shares
 }
 
-func evaluatePolynomial(polynomial []int, point int) int {
+func evaluatePolynomial(polynomial []int, point int) Point {
 	var result int
 	for i := 0; i < len(polynomial); i++ {
-		result += polynomial[i] * powInt(point, i)
+		result += polynomial[i] * int(math.Pow(float64(point), float64(i)))
 	}
-	return result
-}
-
-func powInt(x, y int) int {
-	return int(math.Pow(float64(x), float64(y)))
+	return Point{X: point, Y: result}
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	//rand.Seed(time.Now().UnixNano())
 	fmt.Println("Let's get started")
 	fmt.Println(makeShares(1234, 2, 10))
 }
