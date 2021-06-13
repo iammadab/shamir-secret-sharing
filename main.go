45fdffb4c548bbbfcabd40708b97d3d53df18759
iammadab <iammadab@gmail.com>
2021-06-13 07:52:56 +0000

    Setup make shares function signature

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+)
+
+func makeShares(secret int, minimum int, shares int) []int {
+  return []int{}
+}
+
+func main(){
+  fmt.Println("Let's get started")
+  fmt.Println(makeShares(1234, 2, 10))
+}
