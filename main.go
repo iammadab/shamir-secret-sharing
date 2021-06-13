cf21da5dbb712258df5683d47c6dd2469958fe0f
iammadab <iammadab@gmail.com>
2021-06-13 08:09:57 +0000

    Generating polynomial based on minimum shares

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
+// TODO: Use a more secure random number generator
 func makeShares(secret int, minimum int, shares int) []int {
-  return []int{}
+	var polynomial = make([]int, minimum+1)
+	polynomial[0] = secret
+	for i := 0; i < minimum; i++ {
+		polynomial[i+1] = rand.Intn(secret)
+	}
+	fmt.Println(polynomial)
+	return []int{}
 }
 
-func main(){
-  fmt.Println("Let's get started")
-  fmt.Println(makeShares(1234, 2, 10))
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("Let's get started")
+	fmt.Println(makeShares(1234, 2, 10))
 }
